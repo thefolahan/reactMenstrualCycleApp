b504f8efcda714ed1a993c67763fde1fc7667163
9KICKS <officialjoshua9@gmail.com>
2023-04-28 11:03:07 +0100

    "updated the app"

--- src/pages/dashboard/components/Consultancy.jsx
+++ src/pages/dashboard/components/Consultancy.jsx
@@ -7,11 +7,11 @@ function Consultancy() {
             <div className="appointment">
                 Book an appointment with a gynecologist near you:
             </div>
-            <li><a href="https://southshorewch.com/contacts/" className="clinic">South Shore Women's and Children's Hospital</a></li>
-            <li><a href="https://bodet-medicare-maternity-center.business.site/?utm_source=gmb&utm_medium=referralv" className="clinic">Bodet Medicare Maternity center</a></li>
-            <li><a href="https://clinixhealthcare.com.ng/" className="clinic">Clinix Diagnostic Centre</a></li>
-            <li><a href="https://www.lifebridgeclinic.com/page/" className="clinic">Lifebridge Clinic</a></li>
-            <li><a href="https://medison-specialist-womens-hospital-fertility.business.site/" className="clinic">Medison Specialist Women's Hospital</a></li>
+            <li><a href="https://southshorewch.com/contacts/" className="clinic" target='_blank' rel="noreferrer">South Shore Women's and Children's Hospital</a></li>
+            <li><a href="https://bodet-medicare-maternity-center.business.site/?utm_source=gmb&utm_medium=referralv" className="clinic" target='_blank' rel="noreferrer">Bodet Medicare Maternity center</a></li>
+            <li><a href="https://clinixhealthcare.com.ng/" className="clinic" target='_blank' rel="noreferrer">Clinix Diagnostic Centre</a></li>
+            <li><a href="https://www.lifebridgeclinic.com/page/" className="clinic" target='_blank' rel="noreferrer">Lifebridge Clinic</a></li>
+            <li><a href="https://medison-specialist-womens-hospital-fertility.business.site/" className="clinic" target='_blank' rel="noreferrer">Medison Specialist Women's Hospital</a></li>
             <button onClick={() => window.location.href="/home"}>Home</button>
         </div>
     )
